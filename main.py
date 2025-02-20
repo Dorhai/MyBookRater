8096023e6b2bbce1654dd1d6eb5656b08e4e1921
Dorhai <dorabookasis@gmail.com>
2025-02-20 13:59:58 +0200

    Added book entry in DB while printing it. Added base.html as a template for other pages Added index.html to display book entry

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import requests
 
 URL = "https://openlibrary.org/"
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///book.db"
+
+bootstrap = Bootstrap5(app)
+db = SQLAlchemy(app)
+print(Bootstrap5)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    year = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.String(500), nullable=True)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    result = db.session.execute(db.select(Book))
+    all_books = result.scalars().all()
+    print(all_books)
+    return render_template("index.html", books=all_books)
 
 
 @app.route("/edit", methods=["POST", "GET"])
